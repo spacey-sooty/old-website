b5d9e876008a61ed6c802eb78632397b07dc4dcb
Isaac Turner <spacey_sooty@outlook.com>
2023-07-11 11:38:32 +0800

    improved navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,23 @@
 "use client"
 import React from "react";
 import styles from '@/styles/components/navbar.module.scss'
-import NavLink from "./navlink";
+import Link from 'next/link'
+
+function isActive(url: string): string {
+  var root = window.location.host;
+  url = "http://" + root + url
+  var className = (url == window.location.href) ? styles.active : styles.inactive;
+  return className;
+}
+
+function NavLink(input: {title: string, url: string}) {
+  return (
+      <Link href={input.url} className={isActive(input.url)} onClick={window.location.reload}>
+        {input.title}
+      </Link>
+  )
+}
+
 
 
 const navbarData = {
@@ -27,9 +43,9 @@ export default class Navbar extends React.Component {
       <div className={styles.navbar}>
         <>
           <div>
-            <NavLink title="Home" url="/" />
+            <NavLink title="Home" url="/"/>
           </div>
-          <div>
+          <div className={styles.right}>
             {navbarData.pages.map((page) => (
               <NavLink key={page.title} title={page.title} url={page.url} />
             ))}
