51d0e77040fc5da93283970811aa8ac88343f3af
Isaac Turner <spacey_sooty@outlook.com>
2023-08-12 22:36:12 +0800

    some style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,24 @@
 import styles from '@/styles/pages/home.module.scss'
 import Image from 'next/image'
 
+// export async function getProps(): Promise<{ props: { hello: Promise<string>; }; }> {
+// export async function getProps(): Promise<any> {
 export async function getProps() {
-  const hello = await fetch(process.env.URL + "/api/hello");
+  try {
+    const hello: any = await fetch(process.env.URL + "/api/hello");
 
-  if (!hello) {
-    throw new Error("Failed to fetch data!");
-  }
-
-  return {
-    props: {
-      hello: hello.text()
+    if (!hello) {
+      throw new Error("Failed to fetch data!");
     }
+
+    var props : typeof hello = hello;
+
+    // return { props: { hello: hello.text() } };
+    return props.text();
+  } catch(err) {
+    console.log(err);
   }
-}
+};
 
 export default async function Home() {
   const w = 794 * 0.4;
@@ -25,8 +30,8 @@ export default async function Home() {
     <div className='hero'>
       <div>
         <div>
-          {/* <h1 className={styles.heading}> Hi I&apos;m Isaac Turner! </h1> */}
-          <h1 className={styles.heading}> {data.props.hello} </h1>
+          <h1 className={styles.heading}> Hi I&apos;m Isaac Turner! </h1>
+          {/* <h1 className={styles.heading}> {data.props.hello} </h1> */}
           <p className={styles.pronouns}> They / Them </p>
         </div>
         <Image src="/me.png" alt="Isaac Turner" width={w} height={h} className={styles.image} priority />
