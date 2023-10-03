1ad7e081200637c167fd0510624f262f0bc08e62
Isaac Turner <spacey_sooty@outlook.com>
2023-10-03 13:52:51 +0800

    added blog page

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -6,5 +6,6 @@
   <a href="/" class={styles.home}> Home </a>
   <a href="/about" class={styles.about}> About </a>
   <a href="/portfolio" class={styles.portfolio}> Portfolio </a>
+  <a href="/blog" class={styles.blog}> Blog </a>
 </nav>
 
